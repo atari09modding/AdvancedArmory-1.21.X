b98529fad5957785aea1c79d00af3d925e5556c3
atari09modding <atari09.modding@gmail.com>
2026-05-26 19:07:33 +0200

    changed some things

--- src/main/java/net/atari09/atarisadvancedarmory/screen/ModMenuTypes.java
+++ src/main/java/net/atari09/atarisadvancedarmory/screen/ModMenuTypes.java
@@ -1,6 +1,30 @@
 package net.atari09.atarisadvancedarmory.screen;
 
+import net.atari09.atarisadvancedarmory.AtarisAdvancedArmory;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.common.extensions.IMenuTypeExtension;
+import net.neoforged.neoforge.network.IContainerFactory;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 public class ModMenuTypes {
 
+    public static final DeferredRegister<MenuType<?>> MENUS =
+            DeferredRegister.create(Registries.MENU, AtarisAdvancedArmory.MOD_ID);
 
+
+
+
+
+    private static <T extends AbstractContainerMenu> DeferredHolder<MenuType<?>, MenuType<T>> registerMenuType(String name,
+                                                                                                               IContainerFactory<T> factory){
+        return MENUS.register(name, ()-> IMenuTypeExtension.create(factory));
+    }
+
+    public static void register(IEventBus eventBus){
+        MENUS.register(eventBus);
+    }
 }
