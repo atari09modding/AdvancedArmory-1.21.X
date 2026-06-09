869ec826fbe512cb28f53de4db76ae318d3f38a9
atari09modding <atari09.modding@gmail.com>
2026-06-09 11:53:06 +0200

    changed some things

--- src/main/java/net/atari09/atarisadvancedarmory/entity/ModEntities.java
+++ src/main/java/net/atari09/atarisadvancedarmory/entity/ModEntities.java
@@ -1,17 +1,24 @@
 package net.atari09.atarisadvancedarmory.entity;
 
 import net.atari09.atarisadvancedarmory.AtarisAdvancedArmory;
+import net.atari09.atarisadvancedarmory.entity.custom.BlockProjectileEntity;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.function.Supplier;
+
 public class ModEntities {
-    public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(Registries.ENTITY_TYPE, AtarisAdvancedArmory.MOD_ID);
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(Registries.ENTITY_TYPE, AtarisAdvancedArmory.MOD_ID);
+
+    public static final Supplier<EntityType<BlockProjectileEntity>> BLOCK_PROJECTILE_ENTITY = ENTITY_TYPES.register("block_projectile_entity",
+            ()->EntityType.Builder.<BlockProjectileEntity>of(BlockProjectileEntity::new, MobCategory.MISC).sized(1,1).build("block_projectile_entity"));
 
 
 
     public static void register(IEventBus eventBus){
-        ENTITIES.register(eventBus);
+        ENTITY_TYPES.register(eventBus);
     }
 }
--- src/main/java/net/atari09/atarisadvancedarmory/entity/custom/BlockProjectileEntity.java
+++ src/main/java/net/atari09/atarisadvancedarmory/entity/custom/BlockProjectileEntity.java
@@ -1,11 +1,27 @@
 package net.atari09.atarisadvancedarmory.entity.custom;
 
+import net.atari09.atarisadvancedarmory.entity.ModEntities;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.windcharge.AbstractWindCharge;
+import net.minecraft.world.entity.projectile.windcharge.WindCharge;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 
-public class BlockProjectileEntity extends AbstractWindCharge {
-    @Override
-    protected void explode(Vec3 vec3) {
+public class BlockProjectileEntity extends WindCharge {
+    private BlockState state;
 
+    public BlockProjectileEntity(Player player, Level level, double x, double y, double z) {
+        super(player, level, x, y, z);
     }
+
+
+    @Override
+    protected void explode(Vec3 pos) {
+        this.level().explode(this,pos.x,pos.x,pos.z,5f, Level.ExplosionInteraction.BLOCK);
+    }
+
+
 }
