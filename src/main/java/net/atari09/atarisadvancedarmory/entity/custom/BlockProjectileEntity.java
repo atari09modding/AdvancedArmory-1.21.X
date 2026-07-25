a2ca37c66a1d7e0ac2bb63b3503c8c020514a288
atari09modding <atari09.modding@gmail.com>
2026-07-25 14:09:51 +0200

    changed some things

--- src/main/java/net/atari09/atarisadvancedarmory/entity/custom/BlockProjectileEntity.java
+++ src/main/java/net/atari09/atarisadvancedarmory/entity/custom/BlockProjectileEntity.java
@@ -5,6 +5,7 @@ import net.atari09.atarisadvancedarmory.entity.ModEntities;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
@@ -74,9 +75,10 @@ public class BlockProjectileEntity extends WindCharge {
 
                     ProjectileDeflection deflection1 = (a,b,c)->{
                         deflection.deflect(a,b,c);
-                        a.addDeltaMovement(new Vec3(c.nextInt(-200,200)*0.01d,c.nextInt(-200,200)*0.01d, c.nextInt(-200,200)*0.01d));
+                        a.addDeltaMovement(new Vec3(c.nextInt(-100,100)*0.001d,c.nextInt(-100,100)*0.001d, c.nextInt(-100,100)*0.001d));
                     };
-                    entity1.deflectWithoutSplit(deflection1,entity,owner,deflectedByPlayer);
+
+                    deflection1.deflect(entity1,entity, RandomSource.createNewThreadLocalInstance());
                 }
             }
             this.kill();
@@ -84,9 +86,6 @@ public class BlockProjectileEntity extends WindCharge {
         return super.deflect(deflection, entity, owner, deflectedByPlayer);
     }
 
-    public void deflectWithoutSplit(ProjectileDeflection deflection, @Nullable Entity entity, @Nullable Entity owner, boolean deflectedByPlayer){
-        super.deflect(deflection, entity, owner, deflectedByPlayer);
-    }
 
     public double getSize() {
         return size;
