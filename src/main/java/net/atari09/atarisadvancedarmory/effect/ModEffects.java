77857d26b79bd339704392888017e061fdc56785
atari09modding <atari09.modding@gmail.com>
2026-08-04 16:22:44 +0200

    changed some things

--- src/main/java/net/atari09/atarisadvancedarmory/effect/ModEffects.java
+++ src/main/java/net/atari09/atarisadvancedarmory/effect/ModEffects.java
@@ -29,7 +29,7 @@ public class ModEffects {
                             AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
 
     public static final Holder<MobEffect> VOID_EFFECT = MOB_EFFECTS.register("void",
-            ()->new FreezeEffect(MobEffectCategory.HARMFUL,0xFFD0FF)
+            ()->new VoidEffect(MobEffectCategory.HARMFUL,0xFFD0FF)
                     .addAttributeModifier(Attributes.MAX_HEALTH,
                             AtarisAdvancedArmory.res("void"),-2, AttributeModifier.Operation.ADD_VALUE));
 
